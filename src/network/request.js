44b9787685fdf3dc63212703c222f4daa91a684c
1119864054 <1119864054@qq.com>
2021-02-25 18:23:22 +0800

    登录注册 页面

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export function request(config) {
   // 创建axios实例
   const instance = axios.create({
-    baseURL: "http://127.0.0.1:80"
+    baseURL: ""
   });
   // 发送网络请求
   return instance(config);
