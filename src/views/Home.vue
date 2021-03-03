fd51070bfda4387308f9cf71bb9d4659cdaae07c
1119864054 <1119864054@qq.com>
2021-03-03 18:20:11 +0800

    主页

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-affix :offset="120">
+    <el-button type="primary">距离顶部 120px</el-button>
+  </el-affix>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style scoped>
+
+</style>
